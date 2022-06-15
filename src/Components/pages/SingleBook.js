fbd72b2b8d7a72fd8a3a0e8fe215a33c3bbb0e5a
bizip <bizip04@gmail.com>
2022-06-15 23:34:34 +0200

    Display Data from redux

--- src/Components/pages/SingleBook.js
+++ src/Components/pages/SingleBook.js
@@ -1,5 +1,6 @@
+import PropTypes from 'prop-types';
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux/es/exports';
+import { useDispatch } from 'react-redux/es/exports';
 import { removeBookFunction } from '../../redux/books/books';
 import classes from '../../Styles.module.css';
 
@@ -8,29 +9,39 @@ const propTypes = {};
 const defaultProps = {};
 
 const SingleBook = (props) => {
-    const dispatch = useDispatch();
-    const { item } = props;
-    const handleDelete = (e) => {
-        const id = e.target.id;
-        dispatch(removeBookFunction(id));
-    };
-    return (
+  const dispatch = useDispatch();
+  const { item } = props;
+  const handleDelete = (e) => {
+    const { id } = e.target;
+    dispatch(removeBookFunction(id));
+  };
+  return (
 
-        <li className={classes.booklist}>
-            <div>
-                <div>
-                    {/* <p>{item.category}</p> */}
-                    <h1>{item.title}</h1>
-                    <h3>{item.author}</h3>
-                    <div>
-                        <button type="button">Comments</button>
-                        <button type="button" id={item.id} onClick={handleDelete}>Remove</button>
-                        <button type="button">Edit</button>
-                    </div>
-                </div>
-            </div>
-        </li>
-    );
+    <li className={classes.booklist}>
+      <div>
+        <div>
+          <p>{item.category}</p>
+          <h1>{item.title}</h1>
+          <h3>{item.author}</h3>
+          <div>
+            <button type="button">Comments</button>
+            <button type="button" id={item.id} onClick={handleDelete}>Remove</button>
+            <button type="button">Edit</button>
+          </div>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+SingleBook.propTypes = {
+  // eslint-disable-next-line react/require-default-props
+  item: PropTypes.shape({
+    author: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+  }),
 };
 
 SingleBook.propTypes = propTypes;
